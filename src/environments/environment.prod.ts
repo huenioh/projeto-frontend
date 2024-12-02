00c7290a17209f8513f057108e9208d9c8e1de00
huenioh <hueniohugomoura@gmail.com>
2024-12-02 00:37:01 -0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+    production: true,
+    googleBooksApiKey: 'AIzaSyCKo3wLwesMTLg8h0_my2exkynawn2D3AA'
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    googleBooksApiKey: 'AIzaSyCKo3wLwesMTLg8h0_my2exkynawn2D3AA'
+  };
+  
